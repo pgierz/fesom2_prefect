bd4f20372c22bd223812076d667ecab2c6842fc4
Paul Gierz <pgierz@awi.de>
2022-03-08 10:39:02 +0100

    feat: compile metis and FESOM works in parallel

--- fesom2_prefect/compile_fesom2.py
+++ fesom2_prefect/compile_fesom2.py
@@ -4,13 +4,14 @@ import prefect
 from git import Repo
 from prefect import Flow, Parameter, task
 from prefect.tasks.shell import ShellTask
+from prefect.executors import LocalDaskExecutor
 
 
-@task
+@task(log_stdout=True)
 def download_fesom2(branch, local_location):
     local_location += "/fesom2"
     print(
-        f"We will now download and compile FESOM2 for {branch} to {local_location}/fesom2!"
+        f"We will now download and compile FESOM2 for {branch} to {local_location}!"
     )
     Repo.clone_from(
         "https://github.com/FESOM/fesom2",
@@ -19,21 +20,21 @@ def download_fesom2(branch, local_location):
         ],
         to_path=local_location,
     )
-    return local_location
+    return f"{local_location}"
 
 
-@task
+@task(log_stdout=True)
 def compile_fesom2_ogcm(fesom_folder):
-    logger = prefect.context.get("logger")
-    logger.info("Compiling the main FESOM2 Model")
-    task = ShellTask(helper_script=f"cd {fesom_folder}/fesom2", return_all=True)
-    modules_loaded = task.run(command="module list")
-    logger.info(modules_loaded)
+    print(f"Compiling the FESOM 2 Ocean Model in {fesom_folder}")
+    task = ShellTask(return_all=True, log_stderr=True, helper_script=f"module purge && module list && cd {fesom_folder}")
+    compile_output = task.run(command="bash configure.sh")
 
 
-@task
+@task(log_stdout=True)
 def compile_fesom2_metis(fesom_folder):
     print("Compiling the METIS FESOM2 Mesh Partitioner")
+    task = ShellTask(return_all=True, log_stderr=True, helper_script=f"module purge && module list && cd {fesom_folder}/mesh_part")
+    compile_output = task.run(command="bash configure.sh")
 
 
 with Flow("Compile Fesom2 Model") as compile_model_flow:
@@ -50,7 +51,7 @@ with Flow("Compile Fesom2 Mesh Part") as compile_mesh_part_flow:
     compile_fesom2_metis(fesom_folder)
 
 
-with Flow("Compile Fesom2 All") as compile_all_flow:
+with Flow("Compile Fesom2 All", executor=LocalDaskExecutor()) as compile_all_flow:
     branch = Parameter("Branch Name", default="master")
     local_location = Parameter("Supercomputer Directory", default=os.getcwd())
     fesom_folder = download_fesom2(branch, local_location)
